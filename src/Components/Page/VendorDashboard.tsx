535ffb004a9b064987fb4e48647f1eb03014980c
saiful10400 <saifulislam10400i5@gmail.com>
2025-08-15 18:37:07 +0600

    vendor dashboard asidebar ui updated.

--- src/Components/Page/VendorDashboard.tsx
+++ src/Components/Page/VendorDashboard.tsx
@@ -13,7 +13,7 @@ const VendorDashboard = () => {
     const[showAsideBar,setShowAsideBar]=useState(false)
   return (
     <div className="flex flex-col lg:flex-row items-start gap-10 bg-[#f1f5f9]">
-      <div className="lg:w-[15%] fixed lg:sticky top-0">
+      <div className="lg:w-[15%] fixed z-30 lg:sticky top-0">
         <VendorDashboardAside setterFn={setShowAsideBar} setterState={showAsideBar}/>
       </div>
       <div className="lg:w-[80%]  px-3 w-full min-h-screen flex flex-col gap-7 lg:px-10">
