c6a0b1d547400a68bb4bf1ad93dd3e64529805bf
saiful10400 <saifulislam10400i5@gmail.com>
2025-08-15 18:34:20 +0600

    loader ui fixed.

--- src/Components/Ui/Loading.tsx
+++ src/Components/Ui/Loading.tsx
@@ -20,7 +20,7 @@ setCurrentUser()
 
   if (loadingStatus) {
     return (
-      <div className="w-full h-screen absolute top-0 left-0 bg-[#f0efef75] flex justify-center items-center">
+      <div className="w-full h-screen fixed  top-0 left-0 z-50 bg-[#f0efef75] flex justify-center items-center">
         <div className="w-[150px] h-[150px] rounded-3xl bg-white flex justify-center items-center">
           <img className="w-[100px]" src={loading} alt="" />
         </div>
